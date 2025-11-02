4c1e32c437787c99834b163c9dd03c20dd162bff
Aman Parida <amantechz467@gmail.com>
2025-11-02 21:20:12 +0530

    Access Modifiers

--- L-06/classes.js
+++ L-06/classes.js
@@ -1,17 +1,24 @@
 class Department {
     constructor(name) {
+        this.id = "d1"; // Read only (After this, cannot be assigned)
         this.name = name;
+        this.employees = [];
     }
     describe() {
         console.log("Department = ", this.name);
     }
+    addEmployee(emp) {
+        this.employees.push(emp);
+    }
+    printEmployeeInfo() {
+        console.log("Number of employees: ", this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
 accounting.describe();
-const accountingCopy = {
-    // name: "Aman",
-    describe: accounting.describe
-};
-console.log(accountingCopy.describe());
+accounting.addEmployee("Ram");
+accounting.addEmployee("Hari");
+accounting.printEmployeeInfo();
 export {};
 //# sourceMappingURL=classes.js.map
--- L-06/classes.ts
+++ L-06/classes.ts
@@ -1,18 +1,26 @@
 class Department {
+    private readonly id: string; // Only once can be assigned
     name: string;
+    private employees: string[];
     constructor(name: string) {
+        this.id = "d1"; // Read only (After this, cannot be assigned)
         this.name = name;
+        this.employees = [];
     }
     describe() {
         console.log("Department = ", this.name);
+    }
+    addEmployee(emp: string) {
+        this.employees.push(emp);
+    }
+    printEmployeeInfo() {
+        console.log("Number of employees: ", this.employees.length);
+        console.log(this.employees);
     }
 }
 
 const accounting = new Department('Accounting');
 accounting.describe();
-
-const accountingCopy = {
-    // name: "Aman",
-    describe: accounting.describe
-}
-console.log(accountingCopy.describe());
+accounting.addEmployee("Ram");
+accounting.addEmployee("Hari");
+accounting.printEmployeeInfo();
