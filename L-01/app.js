c94f2dcf2c85f6d42566e433ef55077c588a7e0a
Aman Parida <amantechz467@gmail.com>
2025-11-02 11:52:32 +0530

    basic datatypes

---
+++ L-01/app.js
@@ -0,0 +1,6 @@
+var num = 10;
+console.log(10 + num);
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(10, 20);
---
+++ L-01/app.ts
@@ -0,0 +1,8 @@
+const num: number = 10;
+console.log(10 + num);
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+add(10, 20);
