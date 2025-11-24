0b007f061e145cf88a1abadf98ba93bf0cbfb30b
Aman Parida <amantechz467@gmail.com>
2025-11-24 16:44:41 +0530

    Getter, Setter, Static

--- L-06/classes.js
+++ L-06/classes.js
@@ -1,11 +1,13 @@
+// Inheritance
 class Department {
-    constructor(name) {
-        this.id = "d1"; // Read only (After this, cannot be assigned)
+    constructor(id, name) {
+        this.id = id;
+        this.id = id; // Read only (After this, cannot be assigned)
         this.name = name;
         this.employees = [];
     }
     describe() {
-        console.log("Department = ", this.name);
+        console.log(`Department : (${this.id}, ${this.name})`);
     }
     addEmployee(emp) {
         this.employees.push(emp);
@@ -15,10 +17,48 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department('Accounting');
-accounting.describe();
-accounting.addEmployee("Ram");
-accounting.addEmployee("Hari");
-accounting.printEmployeeInfo();
+// const accounting = new Department('Accounting');
+// accounting.describe();
+// accounting.addEmployee("Ram");
+// accounting.addEmployee("Hari");
+// accounting.printEmployeeInfo();
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+    }
+    addReports(text) {
+        this.reports.push(text);
+    }
+    viewRepoorts() {
+        console.log("Total Reports : ", this.reports.length);
+        console.log(this.reports);
+    }
+    // Override Property
+    addEmployee(emp) {
+        if (emp === "Aman") {
+            return;
+        }
+        this.employees.push(emp);
+    }
+    get getReports() {
+        if (this.reports.length > 0) {
+            return this.reports;
+        }
+        throw new Error("Reports not found!");
+    }
+    set setReports(value) {
+        if (!value) {
+            throw new Error("Please pass valid value");
+        }
+        this.reports.push(value);
+    }
+}
+const accDept = new AccountingDepartment("01", ["Data"]);
+accDept.addReports("First");
+accDept.addReports("Second");
+// Getter & Setter
+console.log(" Getter -> ", accDept.getReports);
+accDept.setReports = "Error is encountered";
 export {};
 //# sourceMappingURL=classes.js.map
--- L-06/classes.ts
+++ L-06/classes.ts
@@ -1,14 +1,15 @@
+// Inheritance
+
 class Department {
-    private readonly id: string; // Only once can be assigned
     name: string;
-    private employees: string[];
-    constructor(name: string) {
-        this.id = "d1"; // Read only (After this, cannot be assigned)
+    protected employees: string[];
+    constructor(private id: string, name: string) {
+        this.id = id; // Read only (After this, cannot be assigned)
         this.name = name;
         this.employees = [];
     }
     describe() {
-        console.log("Department = ", this.name);
+        console.log(`Department : (${this.id}, ${this.name})`);
     }
     addEmployee(emp: string) {
         this.employees.push(emp);
@@ -19,8 +20,55 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting');
-accounting.describe();
-accounting.addEmployee("Ram");
-accounting.addEmployee("Hari");
-accounting.printEmployeeInfo();
+// const accounting = new Department('Accounting');
+// accounting.describe();
+// accounting.addEmployee("Ram");
+// accounting.addEmployee("Hari");
+// accounting.printEmployeeInfo();
+
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, "Accounting");
+    }
+    addReports(text: string) {
+        this.reports.push(text);
+    }
+    viewRepoorts() {
+        console.log("Total Reports : ", this.reports.length);
+        console.log(this.reports);
+    }
+
+    // Override Property
+    addEmployee(emp: string): void {
+        if (emp === "Aman") {
+            return;
+        }
+        this.employees.push(emp);
+    }
+
+    get getReports() {
+        if (this.reports.length > 0) {
+            return this.reports;
+        }
+        throw new Error("Reports not found!");
+    }
+
+    set setReports(value: string) {
+        if (!value) {
+            throw new Error("Please pass valid value");
+        }
+        this.reports.push(value);
+    }
+
+}
+
+const accDept = new AccountingDepartment("01", ["Data"]);
+accDept.addReports("First");
+accDept.addReports("Second");
+
+// Getter & Setter
+console.log(" Getter -> ", accDept.getReports);
+accDept.setReports = "Error is encountered";
+
+
+ 
