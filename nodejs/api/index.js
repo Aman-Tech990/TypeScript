22a6220d4c8d2bb04d8d674ee113bd6cc4d35928
Aman Parida <amantechz467@gmail.com>
2025-11-25 20:57:22 +0530

    Wrapped app for vercel

--- nodejs/api/index.js
+++ nodejs/api/index.js
@@ -2,13 +2,20 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-    res.status(200).json({ success: true, message: "Hello from Vercel backend!" });
+app.get("/", (req, res) => {
+    res
+        .status(200)
+        .json(
+            {
+                success: true,
+                message: "Hello from Server!"
+            }
+        );
 });
 
-// Export the handler for Vercel
 export default app;
